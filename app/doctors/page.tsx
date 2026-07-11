010dda6eb3d3c2aea004ed644545078f2cb7629a
Olivia <oliviasikder2005@gmail.com>
2026-07-12 00:21:33 +0530

    Upgrade Next.js to 16.2.10

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -1,14 +1,12 @@
-"use server"
 
 import { Doctors } from '@/constants'
 import React from 'react'
-import DoctorsDeatails from '../../components/DoctorsDetails'
 import DoctorsDetails from '../../components/DoctorsDetails'
 
-const page = () => {
+const page = async () => {
   return (
     <main>
-        <DoctorsDetails/>
+      <DoctorsDetails />
     </main>
   )
 }
